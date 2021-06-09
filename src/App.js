d6ef9b8835400cfb69e82f704da8de8004317295
Raúl Navarro <raul.navarro.uribe@gmail.com>
2021-06-09 18:35:57 +0200

    Cambios arrays con maps

--- src/App.js
+++ src/App.js
@@ -7,13 +7,15 @@ function App() {
     "Noticias de ocio",
     "Noticias de política",
     "Noticias de espectáculos",
-  ];
-  const otrasCosas = ["Nosequé", "Nosecuanto", "Talycual"];
+  ].map((categoria) => <li>{categoria}</li>);
+  const otrasCosas = ["Nosequé", "Nosecuanto", "Talycual"].map((otraCosa) => (
+    <li>{otraCosa}</li>
+  ));
   const footer = true;
   const cuerpo = [
     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi numquam, iusto omnis minima corrupti est officiis natus nam, adipisci commodi quo autem magnam sequi magni odio soluta temporibus velit quas!",
     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio sequi, dolorum autem alias commodi molestiae facere quibusdam beatae nulla error, dignissimos, libero vitae ratione porro. Delectus dolorem ad non esse.",
-  ];
+  ].map((parrafo) => <p>{parrafo}</p>);
   return (
     <>
       <div className="contenedor-general container">
@@ -23,32 +25,22 @@ function App() {
         <main className="contenedor-principal row">
           <article className="bloque principal col-8">
             <h2 className="titular-noticia">{titularNoticia}</h2>
-            <p className="mt-3">{cuerpo[0]}</p>
-            <p className="mt-3">{cuerpo[1]}</p>
+            {cuerpo}
           </article>
           <aside className="bloque sidebar col-4">
             <section className="listado">
               <h3 className="titular-enlaces">Categorías</h3>
-              <ul className="enlaces">
-                <li>{categorias[0]}</li>
-                <li>{categorias[1]}</li>
-                <li>{categorias[2]}</li>
-                <li>{categorias[3]}</li>
-              </ul>
+              <ul className="enlaces">{categorias}</ul>
             </section>
             <section className="listado">
               <h3 className="titular-enlaces">Otras cosas</h3>
-              <ul className="enlaces">
-                <li>{otrasCosas[0]}</li>
-                <li>{otrasCosas[1]}</li>
-                <li>{otrasCosas[2]}</li>
-              </ul>
+              <ul className="enlaces">{otrasCosas}</ul>
             </section>
           </aside>
         </main>
         <footer className="row bloque pie">
           Texto del footer
-          {footer ? " - Ningún derecho reservado, copia lo que quieras" : ""}
+          {footer && " - Ningún derecho reservado, copia lo que quieras"}
         </footer>
       </div>
     </>
