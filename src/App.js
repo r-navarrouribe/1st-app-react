697d3794a55d80a3ee32ba83566e006c38bedfb3
Raúl Navarro <raul.navarro.uribe@gmail.com>
2021-06-09 19:28:16 +0200

    Cambio de arrays de strings por objetos con id para key

--- src/App.js
+++ src/App.js
@@ -3,19 +3,29 @@ function App() {
   const tituloPagina = "Título";
   const titularNoticia = "Titular de la noticia";
   const categorias = [
-    "Noticias de deporte",
-    "Noticias de ocio",
-    "Noticias de política",
-    "Noticias de espectáculos",
-  ].map((categoria) => <li>{categoria}</li>);
-  const otrasCosas = ["Nosequé", "Nosecuanto", "Talycual"].map((otraCosa) => (
-    <li>{otraCosa}</li>
-  ));
+    { id: 1, titulo: "Noticias de deporte" },
+    { id: 2, titulo: "Noticias de ocio" },
+    { id: 3, titulo: "Noticias de política" },
+    { id: 4, titulo: "Noticias de espectáculos" },
+  ];
+  const otrasCosas = [
+    { id: 1, titulo: "Nosequé" },
+    { id: 2, titulo: "Nosecuanto" },
+    { id: 3, titulo: "Talycual" },
+  ];
   const footer = true;
   const cuerpo = [
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi numquam, iusto omnis minima corrupti est officiis natus nam, adipisci commodi quo autem magnam sequi magni odio soluta temporibus velit quas!",
-    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio sequi, dolorum autem alias commodi molestiae facere quibusdam beatae nulla error, dignissimos, libero vitae ratione porro. Delectus dolorem ad non esse.",
-  ].map((parrafo) => <p>{parrafo}</p>);
+    {
+      id: 1,
+      contenido:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi numquam, iusto omnis minima corrupti est officiis natus nam, adipisci commodi quo autem magnam sequi magni odio soluta temporibus velit quas!",
+    },
+    {
+      id: 2,
+      contenido:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio sequi, dolorum autem alias commodi molestiae facere quibusdam beatae nulla error, dignissimos, libero vitae ratione porro. Delectus dolorem ad non esse.",
+    },
+  ];
   return (
     <>
       <div className="contenedor-general container">
@@ -25,16 +35,26 @@ function App() {
         <main className="contenedor-principal row">
           <article className="bloque principal col-8">
             <h2 className="titular-noticia">{titularNoticia}</h2>
-            {cuerpo}
+            {cuerpo.map((parrafo) => (
+              <p key={parrafo.id}>{parrafo.contenido}</p>
+            ))}
           </article>
           <aside className="bloque sidebar col-4">
             <section className="listado">
               <h3 className="titular-enlaces">Categorías</h3>
-              <ul className="enlaces">{categorias}</ul>
+              <ul className="enlaces">
+                {categorias.map((categoria) => (
+                  <li key={categoria.id}>{categoria.titulo}</li>
+                ))}
+              </ul>
             </section>
             <section className="listado">
               <h3 className="titular-enlaces">Otras cosas</h3>
-              <ul className="enlaces">{otrasCosas}</ul>
+              <ul className="enlaces">
+                {otrasCosas.map((otraCosa) => (
+                  <li key={otraCosa.id}>{otraCosa.titulo}</li>
+                ))}
+              </ul>
             </section>
           </aside>
         </main>
